bf858aa71aebc30339000ad3d8f19f8b84deb14c
ihsah <ihorsahaydak@gmail.com>
2018-08-06 22:35:11 +0300

    Revert "Initial"
    This reverts commit d50a8b45d8bbba96673594d7e8f7470657ea4b5c.

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var submitForm = function (value) {
-    document.getElementById("nn-form-value").value = value;
-    document.getElementById("nn-form").submit();
-};
-
-var resetData = function (value) {
-    document.getElementById("reset-data").value = 1;
-    document.getElementById("nn-form").submit();
-};
